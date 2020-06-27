8f85faab84ea828ce288eb521fb57e63216c0e5b
dan bloodworth <daniel.bloodworth18@my.northampton.ac.uk>
2020-06-27 22:27:44 +0100

    Setter injection example

--- src/main/java/com/dan/spring/myfirstspring/BinarySearch.java
+++ src/main/java/com/dan/spring/myfirstspring/BinarySearch.java
@@ -1,16 +1,23 @@
 package com.dan.spring.myfirstspring;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 //Tells spring that this is a bean.
 @Component
 public class BinarySearch {
-    //Tells Spring that Algorithm is a dependency for BinarySearch
+
     private final Algorithm algorithm;
 
     public BinarySearch(Algorithm algorithm) {
         this.algorithm = algorithm;
     }
+
+    //Alternatively, you can use setter injection like so
+    /*
+    @Autowired
+    public void setAlgorithm(Algorithm algorithm) {
+        this.algorithm = algorithm;
+    }
+     */
 
     public int binarySearch(int[] numbers, int numberToSearch) {
         int[] sortedNumbers = algorithm.sort(numbers);
